29b46650069e59245028f57ed62cf5123a8d8e19
suraj desai <suraj.desai@borderfree.io>
2021-08-23 18:09:25 +0530

    refactor it

--- scripts/generateIcons.js
+++ scripts/generateIcons.js
@@ -3,17 +3,21 @@ const fs = require('fs')
 
 const newArray = []
 for (const [key, value] of Object.entries(json)) {
-  const newData = value.map((emoji) => ({
-    emoji: emoji.emoji,
-    name: emoji.name,
-    v: emoji.unicode_version,
-  }))
-  if(newArray.v>=4.9){
-    newArray.push({
-      title: key.replace(' & ', '_').replace(' ', '_').toLocaleLowerCase(),
-      data: newData,
-    })
+  const newData = []
+  for (let i = 0; i < value.length; i++) {
+    let emoji = value[i]
+    if (emoji.unicode_version < 4.9)
+      newData.push({ emoji: emoji.emoji, name: emoji.name, v: emoji.unicode_version })
   }
+  // const newData = value.map((emoji) => ({
+  //   emoji: emoji.emoji,
+  //   name: emoji.name,
+  //   v: emoji.unicode_version,
+  // }))
+  newArray.push({
+    title: key.replace(' & ', '_').replace(' ', '_').toLocaleLowerCase(),
+    data: newData,
+  })
 }
 
 fs.writeFile('./src/assets/emojis.json', JSON.stringify(newArray), function (err) {
